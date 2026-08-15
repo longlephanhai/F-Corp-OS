6db30e036e54f3912325777044d3fd206741b1e6
longlephanhai <longlph.23it@gmail.com>
2026-08-16 00:18:55 +0700

    feature:merge sprint-task

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -50,7 +50,7 @@ import { UserSprint } from 'modules/user-sprints/entities/user-sprint.entity';
           Task,
           UserSprint
         ],
-        synchronize: false, // Không nên để true trong môi trường production
+        synchronize: true, 
 
       }),
       inject: [ConfigService],
