acdb4c56e3c0bb3414c496154f4746af24cf5005
longlephanhai <longlph.23it@gmail.com>
2026-08-19 00:43:07 +0700

    fix:synchronize

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -55,7 +55,7 @@ import { ReviewRecord } from 'modules/hr-reviews/entities/review-record.entity';
           ReviewCycle,
           ReviewRecord,
         ],
-        synchronize: false,
+        synchronize: true,
       }),
       inject: [ConfigService],
     }),
